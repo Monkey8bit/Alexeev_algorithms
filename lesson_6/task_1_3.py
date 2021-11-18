9a57b7f5c226d652b674af5a6842b3a7f5cba5dc
Monkey8bit <de_construct@mail.ru>
2021-11-19 03:04:02 +0400

    lesson_6_fix

--- lesson_6/task_1_3.py
+++ lesson_6/task_1_3.py
@@ -5,16 +5,17 @@ from sys import getsizeof  # 72 bytes (function)
 lst = (randint(-100, 100) for i in range(100))  # 112 bytes (generator)
 
 max_neg = -100  # 28 bytes (int)
+max_idx = 0  # 28 bytes (int)
 n = 0  # 28 bytes (int)
-for num in lst:  # num - 28 bytes (int)
+for idx, num in enumerate(lst):  # num - 28 bytes (int), idx - 28 bytes (int)
     if num >= 0:
         continue
     else:
         if num > max_neg:
             max_neg = num
-    n += 1
+            max_idx = idx
 
-print(f"Maximum negative number: {max_neg}.\nIndex of {max_neg}: {n}")
+print(f"Maximum negative number: {max_neg}.\nIndex of {max_neg}: {max_idx}")
 print("-" * 100)
 dir_lst = {k: v for k, v in locals().items() if not k.startswith("__")}
 print(f"Size of al variables: {sum(map(getsizeof, dir_lst.values()))}")
